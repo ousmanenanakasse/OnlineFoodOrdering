ece31f6b03b83f7b3d3820cf6c23b9e1c2894436
ousmanenanakasse <nanakasseo06@gmail.com>
2025-12-04 20:44:28 +0300

    Initial commit

--- src/CreditCardPayment.java
+++ src/CreditCardPayment.java
@@ -1,4 +1,6 @@
 public class CreditCardPayment implements PaymentMethod {
     @Override
-    public void pay(double amount)
+    public void pay(double amount){
+        System.out.println("Paid "+ amount + "TL using credit card.");
+    }
 }
