bdb13c7b09bf69a507cbc5463fb88e02250209b2
ousmanenanakasse <nanakasseo06@gmail.com>
2025-12-04 20:52:51 +0300

    Initial commit

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Restaurant {
+    private List<MenuItem> menu;
 
+    public Restaurant(){
+        this.menu = new ArrayList<>();
+    }
 }
