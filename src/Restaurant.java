68a7ffe66d4686166272644c874e6203effc26d7
ousmanenanakasse <nanakasseo06@gmail.com>
2025-12-05 15:04:58 +0300

    Initial commit

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -6,4 +6,19 @@ public class Restaurant {
     public Restaurant(){
         this.menu = new ArrayList<>();
     }
+    public void addMenuItem(MenuItem item){
+        menu.add(item);
+    }
+    public void displayMenu(){
+        System.out.println("\n---MENU---");
+        for (int i = 0; i < menu.size(); i++ ){
+            System.out.println((i + 1) + " ."+ menu.get(i));
+        }
+    }
+    public MenuItem getItem(int index){
+        if (index >= 0 && index < menu.size()){
+            return menu.get(index);
+        }
+        return
+    }
 }
