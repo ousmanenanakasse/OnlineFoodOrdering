2a582da62cb58cd72da5c4d62b26544ecf983564
ousmanenanakasse <nanakasseo06@gmail.com>
2026-01-01 17:34:31 +0300

    Initial commit

--- src/MenuItemTest.java
+++ src/MenuItemTest.java
@@ -1,5 +1,5 @@
-import org.testng.annotations.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MenuItemTest {
 
